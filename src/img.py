b53e4c9745b485fd5aa1bf3c736af8421544e0b2
mdcharw <anshgarg06@gmail.com>
2023-04-24 13:52:46 +0530

    Pushing Code

--- src/img.py
+++ src/img.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import cv2
 
-img = cv2.imread("../data/images/camera_calibration.jpeg")
+img = cv2.imread("../data/images/camera_calibration2.jpeg")
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 plt.imshow(img)
